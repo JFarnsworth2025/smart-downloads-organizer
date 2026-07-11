6051e9c7315c7b31419d705154fc0183bf49872e
JFarnsworth2025 <jfarnsworthpersonal@gmail.com>
2026-07-10 20:55:40 -0700

    Release v1.0.0 of Smart Downloads Organizer

--- organizer.py
+++ organizer.py
@@ -1,11 +1,22 @@
 from pathlib import Path
+import shutil
+from config import FILE_CATEGORIES
 
 
-downloads_folder = Path.home() / "Downloads"
+downloads_folder = Path.home() / "Downloads" / "Practice"
 
 def organize_downloads():
     for file in downloads_folder.iterdir(): 
-        if file.is_file(): 
-            print(f"📄 {file.name}") 
+        if file.is_file():
+            category = FILE_CATEGORIES.get(file.suffix.lower(), "Other")
+            destination_folder = downloads_folder / category
+            destination_folder.mkdir(exist_ok=True)
+            destination = destination_folder / file.name
+            counter = 1
+            while destination.exists():
+                new_filename = f"{file.stem} ({counter}){file.suffix}"
+                destination = destination_folder / new_filename
+                counter += 1
+            shutil.move(file, destination)
         elif file.is_dir(): 
             print(f"📁 {file.name}")
